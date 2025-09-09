ffcc523bd0aaf8566fb26f64d82b2ac68c8d31b8
Mário Costa <mariocostaa6@gmail.com>
2025-09-09 22:03:21 +0100

    feat: Enhance dashboard with statistics and recent posts

--- app/controllers/panel/dashboard_controller.rb
+++ app/controllers/panel/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class Panel::DashboardController < ApplicationController
+  include HashidHelper
   before_action :authenticate_user!  # protege todas as actions
   layout "panel/panel_layout"
   def index
+    @total_posts = Post.all.count
+    @total_categories = Category.all.count
+    @total_tags = Tag.all.count
+    @total_post_comemts = PostComment.all.count
+    posts = Post.all.order(created_at: :desc).limit(5)
+    @posts = hashify_ids(posts, HASHIDS_TAG)
   end
 end

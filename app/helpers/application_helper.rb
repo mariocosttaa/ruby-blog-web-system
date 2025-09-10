8cc43a4e69b788f7cb9d057fd8b2f3ecfb0f5fdd
Mário Costa <mariocostaa6@gmail.com>
2025-09-10 04:32:36 +0100

    chore: Update application helper with nav active class
    - app/helpers/application_helper.rb
    - app/views/layouts/panel/parts/_navbar.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  include TimeHelper
+
+  # Returns `active_class` if the current controller (and optional action) matches
+  # Usage: nav_active_class('public/home', nil, 'text-purple')
+  #        nav_active_class('public/articles', 'show', 'font-bold')
+  def nav_active_class(controller_name, action_name = nil, active_class = "active")
+    if action_name
+      params[:controller] == controller_name && params[:action] == action_name ? active_class : nil
+    else
+      params[:controller] == controller_name ? active_class : nil
+    end
+  end
 end

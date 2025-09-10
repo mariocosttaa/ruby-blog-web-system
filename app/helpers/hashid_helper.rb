650b7c696970aeda9812e6bebd42c50e62b071bc
Mário Costa <mariocostaa6@gmail.com>
2025-09-10 04:32:51 +0100

    chore: Update hashid helper with hashify_ids method
    - app/helpers/hashid_helper.rb

--- app/helpers/hashid_helper.rb
+++ app/helpers/hashid_helper.rb
@@ -1,16 +1,25 @@
 # app/helpers/hashid_helper.rb
 module HashidHelper
-  # Sobrescreve o `id` de cada objeto da collection
-  def hashify_ids(collection, hashids)
-    if collection.blank? || !hashids.present?
-      collection
-    else
-    collection.each do |record|
-      real_id = record.id
-      record.define_singleton_method(:id) do
-        hashids.encode(real_id)
+  # Sobrescreve o `id` de cada objeto ou objeto único
+  def hashify_ids(data, hashids)
+    return data if data.blank? || !hashids.present?
+
+    if data.respond_to?(:each) # collection (array or ActiveRecord::Relation)
+      data.each do |record|
+        overwrite_id(record, hashids)
       end
+    else # single object
+      overwrite_id(data, hashids)
     end
+    data
+  end
+
+  private
+  # Helper interno para sobrescrever id
+  def overwrite_id(record, hashids)
+    real_id = record.id
+    record.define_singleton_method(:id) do
+      hashids.encode(real_id)
     end
   end
 end
